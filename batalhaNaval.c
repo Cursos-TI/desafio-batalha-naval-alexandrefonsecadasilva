ff868eae7b5aab9802ffb12845db20fd5b8069e0
alexandrefonsecadasilva <alexandrefonsecagg@gmail.com>
2025-08-20 23:10:48 -0300

    adicionado mais navios na diagonal, e exibindo o tabuleiro completo mostrando 0 para agua e 3 para navio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,33 +7,41 @@ int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
     int i, j;
 
-    // 1) Inicializar o tabuleiro com 0 (água)
+    // 1) Inicializar tabuleiro com 0 (água)
     for (i = 0; i < TAMANHO_TABULEIRO; i++) {
         for (j = 0; j < TAMANHO_TABULEIRO; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // 2) Definir os navios como vetores (cada parte do navio = 3)
-    int navio_horizontal[TAMANHO_NAVIO] = {3, 3, 3};
-    int navio_vertical[TAMANHO_NAVIO]   = {3, 3, 3};
+    // 2) Definir coordenadas iniciais dos navios
+    int linhaH = 2, colunaH = 1; // navio horizontal
+    int linhaV = 5, colunaV = 7; // navio vertical
+    int linhaD1 = 0, colunaD1 = 0; // navio diagonal ↘
+    int linhaD2 = 0, colunaD2 = 9; // navio diagonal ↙
 
-    // 3) Definir coordenadas iniciais
-    int linhaH = 2, colunaH = 1; // navio horizontal começa em (2,1)
-    int linhaV = 5, colunaV = 7; // navio vertical começa em (5,7)
-
-    // 4) Posicionar navio horizontal (tamanho fixo = 3)
+    // 3) Posicionar navio horizontal
     for (i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[linhaH][colunaH + i] = navio_horizontal[i];
+        tabuleiro[linhaH][colunaH + i] = 3;
     }
 
-    // 5) Posicionar navio vertical (tamanho fixo = 3)
+    // 4) Posicionar navio vertical
     for (i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[linhaV + i][colunaV] = navio_vertical[i];
+        tabuleiro[linhaV + i][colunaV] = 3;
     }
 
-    // 6) Mostrar tabuleiro
-    printf("Tabuleiro Batalha Naval (0 = agua, 3 = navio)\\n\\n");
+    // 5) Posicionar navio diagonal ↘ (linha++, coluna++)
+    for (i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linhaD1 + i][colunaD1 + i] = 3;
+    }
+
+    // 6) Posicionar navio diagonal ↙ (linha++, coluna--)
+    for (i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linhaD2 + i][colunaD2 - i] = 3;
+    }
+
+    // 7) Exibir tabuleiro completo
+    printf("Tabuleiro 10x10 (0 = agua, 3 = navio)\\n\\n");
     for (i = 0; i < TAMANHO_TABULEIRO; i++) {
         for (j = 0; j < TAMANHO_TABULEIRO; j++) {
             printf("%2d ", tabuleiro[i][j]);
